af785e09f4d74383553944af2827302f8292e807
Sourav Sarkar <sonu.souravsarkar0785@gmail.com>
2026-04-04 13:45:23 +0530

    Initial commit - Customer Churn Prediction Project

---
+++ README.md
@@ -0,0 +1,22 @@
+# Customer Churn Prediction
+
+## 📌 Problem
+Predict whether a customer will churn (leave) or not.
+
+## 🛠 Tools Used
+- Python
+- Pandas
+- Scikit-learn
+
+## ⚙️ Process
+- Data Cleaning
+- Feature Encoding
+- Model Training (Logistic Regression, Random Forest)
+
+## 📊 Results
+- Logistic Regression Accuracy: ~78%
+- Random Forest Accuracy: ~78%
+
+## 🔍 Key Insights
+- Contract type and tenure are major factors affecting churn
+- Customers with higher monthly charges are more likely to churn
---
+++ main.py
@@ -0,0 +1,76 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("Telco-Customer-Churn.csv")
+
+print(df.head())
+print(df.info())
+print(df.describe())
+
+df.drop("customerID", axis=1, inplace=True)
+
+df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
+
+print(df.isnull().sum())
+
+df.dropna(inplace=True)
+
+df["Churn"] = df["Churn"].map({"Yes": 1, "No": 0})
+
+print(df.head())
+print(df.info())
+
+df = pd.get_dummies(df, drop_first=True)
+
+print(df.head())
+print(df.info())
+
+#Train-Test Split
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print(X_train.shape)
+print(X_test.shape)
+
+#scaling the data
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Logistic Regression
+model = LogisticRegression(max_iter=2000)
+model.fit(X_train_scaled, y_train)
+
+y_pred = model.predict(X_test_scaled)
+
+print("Logistic Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+# Random Forest
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+rf.fit(X_train, y_train)
+
+y_pred_rf = rf.predict(X_test)
+
+print("RF Accuracy:", accuracy_score(y_test, y_pred_rf))
+
+importance = pd.Series(rf.feature_importances_, index=X.columns)
+importance = importance.sort_values(ascending=False)
+
+print(importance.head(10))
+
+importance.head(10).plot(kind='barh')
+plt.title("Top 10 Important Features")
+plt.show()
